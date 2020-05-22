946de944310b62bf8e95ca1b99db66d61305f4d2
Jake <jworsham9697@gmail.com>
2020-05-22 07:44:16 -0500

    first commit for lab 21 & 25

---
+++ LAB 21 and LAB 25/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB_21_and_LAB_25.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LAB 21 and LAB 25/Views/Home/Result.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LAB_21_and_LAB_25.Views
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
